6eb50f0378d27d4f81ed895b13b78b38e65a0edf
guerht <parkseunghoon@gpa.ac.kr>
2015-10-01 14:01:10 +0900

    FIRST COMMIT

---
+++ BankAccount.java
@@ -0,0 +1,45 @@
+/*
+ * Project: BankAccount.java
+ * Description: This is a bank account class
+ * Author: Seunghoon Park
+ * Date: 1 October, 2015
+ */
+public class BankAccount {
+	private double balance; // amount of money in the account
+	String name; // the person who owns the account
+	int accPass;
+	public BankAccount() {
+		balance = 0;
+		name = "Dark Templar";
+		accPass = 123456;
+	}
+	public BankAccount(String person, double dollars) {
+		this.balance = dollars;
+		this.name = person;
+		this.accPass = 123456;
+	}
+	public void setPassword(int newPass) {
+		this.accPass = newPass;
+	}
+	public void deposit(double amt) {
+		this.balance += amt;
+	}
+	public void withdraw(double amt) {
+		this.balance -= amt;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public int getBalance(int password) {
+		if(accPass == password) {
+			System.out.println("Account balance: " + balance);
+		}
+		else {
+			System.out.println("Incorrect password.");
+		}
+		return 0;
+	}
+	public int setPassword() {
+		
+	}
+}
---
+++ BankAccountUser.java
@@ -0,0 +1,36 @@
+/*
+ * Project: BankAccountUser.java
+ * Description: This is a bank account user class
+ * Author: Seunghoon Park
+ * Date: 1 October, 2015
+ */
+import java.util.Scanner;
+public class BankAccountUser {
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+		double money;
+		String name;
+		int password;
+		double x, y;
+		// initial money should be $1000
+		System.out.println("Enter the name of the account owner: ");
+		name = scan.nextLine();
+		System.out.println("Enter the amount of money in account: ");
+		money = scan.nextDouble();
+		BankAccount b1 = new BankAccount(name, money);
+		// deposits
+		System.out.println("Enter the amount to deposit: ");
+		x = scan.nextDouble();
+		b1.deposit(x);
+		System.out.println("Check balance? What is the password?");
+		password = scan.nextInt();
+		System.out.println(b1.getBalance(password));
+		// withdraws
+		System.out.println("Please change your password: ");
+		password = scan.nextInt();
+		b1.setPassword(password);
+		System.out.println("Check balance? What is the password?");
+		password = scan.nextInt();
+		System.out.println(b1.getBalance(password));
+	}
+}
